41d7bb1f7c30799a5f4466fc84a797408fc66e31
Zoltan Richard Dudas <zdudas@c1r4p4.42vienna.com>
2026-04-29 21:56:10 +0200

    send_char

--- client.c
+++ client.c
@@ -2,6 +2,26 @@
 #include "libft/libft.h"
 #include <signal.h>
 
+void send_char(int pid, char c)
+{
+	int i = 7;
+	int bit;
+
+	while(i >= 0)
+	{		
+		bit = (c >> i) & 1;
+		if(bit == 0)
+			kill(pid, SIGUSR1);
+		else if(bit == 1)
+			kill(pid, SIGUSR2);
+		usleep(300);
+		i--;
+	}
+}
+
+
+
+
 int main(int argc, char **argv)
 {
 	int pid;
@@ -13,6 +33,6 @@ int main(int argc, char **argv)
 	}
 
 	pid = ft_atoi(argv[1]);
-	kill(pid, SIGUSR1);
-	//kill(pid, SIGUSR2);
+
+	send_char(pid, 'A');
 }
