d436e0f7d87c0698eaa7e3b9965f5de0aaf720bd
Dudee0413 <zoltanricharddudas@gmail.com>
2026-05-04 13:23:01 +0200

    ack

--- client.c
+++ client.c
@@ -1,45 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zdudas <zdudas@student.42vienna.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/27 22:49:25 by zdudas            #+#    #+#             */
+/*   Updated: 2026/04/27 22:49:27 by zdudas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include "libft/libft.h"
 #include <signal.h>
 
-void send_char(int pid, char c)
-{
-	int i = 7;
-	int bit;
+static volatile sig_atomic_t	g_ack = 0;
 
-	while(i >= 0)
-	{		
+void	ack_handler(int sig)
+{
+	(void)sig;
+	g_ack = 1;
+}
+
+void	send_char(int pid, char c)
+{
+	int	i;
+	int	bit;
+
+	i = 7;
+	while (i >= 0)
+	{
+		g_ack = 0;
 		bit = (c >> i) & 1;
-		if(bit == 0)
+		if (bit == 0)
 			kill(pid, SIGUSR1);
-		else if(bit == 1)
+		else
 			kill(pid, SIGUSR2);
-		usleep(300);
+		while (!g_ack)
+			usleep(100);
 		i--;
 	}
 }
 
-
-
-
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int pid;
-	int i;
+	int	pid;
+	int	i;
 
+	signal(SIGUSR1, ack_handler);
 	i = 0;
 	if (argc != 3)
 	{
 		ft_putstr_fd("Error argc is not 3", 1);
-		return(1);
+		return (1);
 	}
-
 	pid = ft_atoi(argv[1]);
-
-	while(argv[2][i])
-	{		
+	while (argv[2][i])
+	{
 		send_char(pid, argv[2][i]);
 		i++;
 	}
-	return(0);
+	send_char(pid, '\0');
+	return (0);
 }
